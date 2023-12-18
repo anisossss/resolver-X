dc5448a8630878cb10f1e6fc931411dcf24c3f69
anis <anis.khalef98@gmail.com>
2023-12-18 05:30:55 +0100

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,20 @@
-import { Montserrat } from 'next/font/google'
-import './globals.css'
-import MainComponent from "@/components/MainComponent"
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+import MainComponent from "@/components/MainComponent";
 
-const montserrat = Montserrat({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'myAssistant',
-  description: 'Create your own GPT Assistant powered by OpenAI',
-}
+  title: "RESOLVER X",
+  description: "YOUR RESOLVER IS RIGHT HERE",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${montserrat.className}`}>
-        <MainComponent children={children}/>
+        <MainComponent children={children} />
       </body>
     </html>
-  )
+  );
 }
